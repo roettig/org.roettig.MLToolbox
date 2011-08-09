0c4be19563ffc2f5afce1ccc16a304dc7be0f9c7
Marc Roettig <marc.roettig@googlemail.com>
2011-08-09 21:25:34 +0200

    cleanup of class CSVCModel.

--- src/org/roettig/MLToolbox/model/CSVCModel.java
+++ src/org/roettig/MLToolbox/model/CSVCModel.java
@@ -4,31 +4,17 @@ package org.roettig.MLToolbox.model;
 import java.util.ArrayList;
 import java.util.List;
 import org.roettig.MLToolbox.base.Prediction;
-import org.roettig.MLToolbox.base.impl.DefaultInstanceContainer;
 import org.roettig.MLToolbox.base.instance.Instance;
 import org.roettig.MLToolbox.base.instance.InstanceContainer;
-import org.roettig.MLToolbox.base.instance.PrimalInstance;
 import org.roettig.MLToolbox.base.label.FactorLabel;
 import org.roettig.MLToolbox.base.label.Label;
 import org.roettig.MLToolbox.base.parameter.Parameter;
 import org.roettig.MLToolbox.kernels.KernelFunction;
 import org.roettig.MLToolbox.kernels.KernelMatrix;
-import org.roettig.MLToolbox.kernels.LinearKernel;
-import org.roettig.MLToolbox.kernels.RBFKernel;
-import org.roettig.MLToolbox.test.data.DataSource;
-import org.roettig.MLToolbox.util.InstanceReader;
 import org.roettig.MLToolbox.util.libsvm.libsvmDelegate;
 import org.roettig.MLToolbox.util.libsvm.svm;
 import org.roettig.MLToolbox.util.libsvm.svm_parameter;
 import org.roettig.MLToolbox.validation.AccuracyMeasure;
-import org.roettig.MLToolbox.validation.FMeasure;
-import org.roettig.MLToolbox.validation.GMeasure;
-import org.roettig.MLToolbox.validation.ModelValidation;
-import org.roettig.MLToolbox.validation.Precision;
-import org.roettig.MLToolbox.validation.QualityMeasure;
-import org.roettig.MLToolbox.validation.Sensitivity;
-import org.roettig.MLToolbox.validation.Specificity;
-
 
 public class CSVCModel<T extends Instance> extends LibsvmModel<T> implements ClassificationModel
 {
@@ -83,105 +69,5 @@ public class CSVCModel<T extends Instance> extends LibsvmModel<T> implements Cla
 			preds.add(pred);
 		}
 		return preds;
-	}
-
-	public static void main(String[] args) throws Exception
-	{
-		/*
-		RBFKernel rbf = new RBFKernel();
-		rbf.setGamma(0.00001);
-		CSVCModel<PrimalInstance> m = new CSVCModel<PrimalInstance>(rbf);
-		
-		List<Parameter<?>> params = m.getParameters();
-		for(Parameter<?> p : params)
-		{
-			System.out.println(p);
-		}
-		*/
-		
-		/*
-		RBFKernel rbf = new RBFKernel();
-		rbf.setGamma(0.01);
-		
-		Label lab1 = new FactorLabel("class1");
-		Label lab2 = new FactorLabel("class2");
-		
-		PrimalInstance pi1 = new PrimalInstance(lab1,new double[]{-0.1});
-		PrimalInstance pi2 = new PrimalInstance(lab1,new double[]{-0.3});
-		PrimalInstance pi3 = new PrimalInstance(lab1,new double[]{-0.1});
-		PrimalInstance pi4 = new PrimalInstance(lab1,new double[]{-0.3});
-		PrimalInstance pi5 = new PrimalInstance(lab2,new double[]{0.1});
-		PrimalInstance pi6 = new PrimalInstance(lab2,new double[]{0.3});
-		PrimalInstance pi7 = new PrimalInstance(lab2,new double[]{0.1});
-		PrimalInstance pi8 = new PrimalInstance(lab2,new double[]{0.3});
-		
-		InstanceView<PrimalInstance> iv = new InstanceView<PrimalInstance>();
-		iv.add(pi1);
-		iv.add(pi2);
-		iv.add(pi3);
-		iv.add(pi4);
-		iv.add(pi5);
-		iv.add(pi6);
-		iv.add(pi7);
-		iv.add(pi8);
-		
-		
-		CSVCModel<PrimalInstance> m = new CSVCModel<PrimalInstance>(rbf);
-		m.train(iv);
-		List<Prediction> preds = m.predict(iv);
-		m.getQuality(preds);
-		
-		double qual = ModelValidation.CV(2, iv, m);
-		System.out.println(qual);
-		*/
-		
-		RBFKernel rbf = new RBFKernel();
-		rbf.setGamma(1.0,0.1,0.01,0.001);
-		
-		LinearKernel lK = new LinearKernel();
-		
-		DefaultInstanceContainer<PrimalInstance>  data = InstanceReader.read(DataSource.class.getResourceAsStream("iris.dat"), 5, true);
-
-		CSVCModel<PrimalInstance>                 m  = new CSVCModel<PrimalInstance>(rbf);
-		m.setQualityMeasure(new GMeasure());
-		
-		m.setC(1.0,100.0);
-		
-		ModelValidation.turnLoggingOff();
-		
-		SelectedModel<PrimalInstance> sm = ModelValidation.SimpleNestedCV(5, 5, data, m);
-		QualityMeasure sens = new Sensitivity();
-		QualityMeasure spec = new Specificity();
-		QualityMeasure prec = new Precision();
-		System.out.println("qual="+sm.qual+" sens="+sens.getQuality(sm.predictions)+" spec="+spec.getQuality(sm.predictions)+" prec="+prec.getQuality(sm.predictions));
-
-		/*
-		DefaultInstanceContainer<PrimalInstance>  train = new DefaultInstanceContainer<PrimalInstance>();
-		DefaultInstanceContainer<PrimalInstance>  test  = new DefaultInstanceContainer<PrimalInstance>();
-		
-		ModelValidation.getStratifiedSplit(0.5, data, train, test);
-		
-		MLHelper.exportLIBSVM(train, "/tmp/iris.trn");
-		MLHelper.exportLIBSVM(test, "/tmp/iris.tst");
-		
-		SelectedModel sm = ModelValidation.ModelSelection(2, train, m);
-		
-		//m.train(train);
-		
-		List<Prediction> preds = sm.model.predict(test);
-		System.out.println(m.getQuality(preds));
-		*/
-		/*
-		ModelValidation.turnLoggingOff();
-		double qual = ModelValidation.CV(5, iv, m);
-		System.out.println(qual);
-		Random rng = new Random(2204);
-		iv.shuffle(rng);
-		
-		qual = ModelValidation.CV(5, iv, m);
-		System.out.println(qual);
-		*/
-		//ModelValidation.ModelSelection(2, data, m);
-	}
-	
+	}	
 }
