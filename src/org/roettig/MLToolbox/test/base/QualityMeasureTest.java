64892343957e43bfc70017aabb4cfc69311629e7
Marc Roettig <marc.roettig@googlemail.com>
2011-08-10 16:00:57 +0200

    added several QualityMeasure tests.

--- src/org/roettig/MLToolbox/test/base/QualityMeasureTest.java
+++ src/org/roettig/MLToolbox/test/base/QualityMeasureTest.java
@@ -6,8 +6,13 @@ import java.util.List;
 import org.roettig.MLToolbox.base.Prediction;
 import org.roettig.MLToolbox.base.label.FactorLabel;
 import org.roettig.MLToolbox.base.label.Label;
+import org.roettig.MLToolbox.validation.FMeasure;
+import org.roettig.MLToolbox.validation.MCC;
+import org.roettig.MLToolbox.validation.Precision;
 import org.roettig.MLToolbox.validation.QualityMeasure;
 import org.roettig.MLToolbox.validation.Sensitivity;
+import org.roettig.MLToolbox.validation.Specificity;
+import org.roettig.maths.statistics.Statistics;
 
 import junit.framework.TestCase;
 
@@ -50,5 +55,86 @@ public class QualityMeasureTest extends TestCase
 		sq = sens.getQuality(preds);
 		// tp=5 fn=1
 		assertEquals(0.83333,sq,1e-3);
+	}
+	
+	public void testSpecificity()
+	{
+		List<Prediction> preds = new ArrayList<Prediction>();
+		
+		FactorLabel pos = new FactorLabel("pos");
+		FactorLabel neg = new FactorLabel("neg");
+		
+		preds.add(new Prediction(null,neg,neg));
+		preds.add(new Prediction(null,pos,pos));
+		preds.add(new Prediction(null,neg,neg));
+		preds.add(new Prediction(null,pos,pos));
+		preds.add(new Prediction(null,neg,pos));
+		preds.add(new Prediction(null,neg,neg));
+		
+		QualityMeasure spec = new Specificity(pos);
+		double sq = spec.getQuality(preds);
+		// tn=3 fp=1
+		assertEquals(0.75,sq,1e-3);
+	}
+	
+	public void testPrecision()
+	{
+		List<Prediction> preds = new ArrayList<Prediction>();
+		
+		FactorLabel pos = new FactorLabel("pos");
+		FactorLabel neg = new FactorLabel("neg");
+		
+		preds.add(new Prediction(null,neg,neg));
+		preds.add(new Prediction(null,pos,pos));
+		preds.add(new Prediction(null,neg,neg));
+		preds.add(new Prediction(null,pos,pos));
+		preds.add(new Prediction(null,neg,pos));
+		preds.add(new Prediction(null,neg,neg));
+		
+		QualityMeasure prec = new Precision(pos);
+		double sq = prec.getQuality(preds);
+		// tp=2 fp=1
+		assertEquals(0.6666666666666666,sq,1e-3);
+	}
+	
+	public void testFMeasure()
+	{
+		List<Prediction> preds = new ArrayList<Prediction>();
+		
+		FactorLabel pos = new FactorLabel("pos");
+		FactorLabel neg = new FactorLabel("neg");
+		
+		preds.add(new Prediction(null,neg,neg));
+		preds.add(new Prediction(null,pos,pos));
+		preds.add(new Prediction(null,neg,neg));
+		preds.add(new Prediction(null,pos,pos));
+		preds.add(new Prediction(null,neg,pos));
+		preds.add(new Prediction(null,neg,neg));
+		
+		QualityMeasure pr = new Precision(pos);
+		QualityMeasure re = new Sensitivity(pos);
+		double  prv = pr.getQuality(preds);
+		double  rev = re.getQuality(preds);
+		System.out.println(prv);
+		System.out.println(rev);
+		QualityMeasure fm = new FMeasure(pos);
+		double sq = fm.getQuality(preds);
+		// tp=2 fp=1 tn=2 fn=0
+		assertEquals(0.8,sq,1e-5);
+	}
+	
+	public void testMCC()
+	{
+		List<Prediction> preds = new ArrayList<Prediction>();
+		FactorLabel pos = new FactorLabel("pos");
+		FactorLabel neg = new FactorLabel("neg");
+		preds.add(new Prediction(null,pos,pos));
+		preds.add(new Prediction(null,pos,pos));
+		preds.add(new Prediction(null,pos,pos));
+		preds.add(new Prediction(null,neg,pos));
+		preds.add(new Prediction(null,neg,pos));
+		preds.add(new Prediction(null,neg,neg));
+		QualityMeasure mcc = new MCC(pos);
+		assertEquals(0.4472135954999579,mcc.getQuality(preds),1e-5);
 	}
 }
